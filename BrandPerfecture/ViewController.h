094bfc693cd9571fa5f9aee25fee092882786c1a
薛焱 <xueyan@xueyandeimac.local>
2016-01-05 13:01:12 +0800

    Initial Commit

---
+++ BrandPerfecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrandPerfecture
+//
+//  Created by 薛焱 on 16/1/5.
+//  Copyright © 2016年 薛焱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BrandPerfectureTests/BrandPerfectureTests.m
@@ -0,0 +1,39 @@
+//
+//  BrandPerfectureTests.m
+//  BrandPerfectureTests
+//
+//  Created by 薛焱 on 16/1/5.
+//  Copyright © 2016年 薛焱. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BrandPerfectureTests : XCTestCase
+
+@end
+
+@implementation BrandPerfectureTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
